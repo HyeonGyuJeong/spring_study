3264c5fe937535a75d36eb057f57dc15f4a32b28
HyeonGyu Jeong <gusrbwjd@gmail.com>
2022-09-21 10:08:14 +0900

    ~ Bean Validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
 import lombok.Data;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "10000원이상 입력")
 public class Item {
 
+//	@NotNull(groups = UpdateCheck.class)
     private Long id;
+    
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+    
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+    
+//    @NotNull
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
